62e327f002449703acf8aa9d2bb6cfd437fd8f59
MiniMath92 <50070281+MiniMath92@users.noreply.github.com>
2019-10-29 11:47:36 +0200

    Test and publications

--- src/test/com.haagahelia/BookStoreApplicationTests.java
+++ src/test/com.haagahelia/BookStoreApplicationTests.java
@@ -1,16 +1,26 @@
-package com.example.demo;
+package com.haagahelia;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.haagahelia.course.web.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookStoreApplicationTests {
 
+	@Autowired
+	private BookController controller;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
